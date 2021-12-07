4bbb55aff1053c9c96051ad91b973cca302ddebc
河田鷹介 <benefium@gmail.com>
2021-12-07 22:39:31 +0900

    出力冒頭に系の情報を出す

--- main.c
+++ main.c
@@ -1,15 +1,34 @@
 #include "graint.h"
 
-void	run(t_system *system, t_system_expander expander, t_system_printer printer)
+void	run(t_system *system, t_system_expander expand_time, t_system_printer print)
 {
 	size_t	i = 0;
 
 	while (i < system->n)
 	{
-		expander(system);
-		printer(system, i);
+		expand_time(system);
+		print(system, i);
 		i += 1;
 	}
+}
+
+void	print_profile(t_system *system)
+{
+	printf("# times:            %zu\n", system->n);
+	printf("# dt:               %1.8f\n", system->dt);
+	printf("# [particle 1]\n");
+	printf("# mass:             %1.8f\n", system->m1);
+	printf("# initial position: (%1.8f, %1.8f, %1.8f)\n", system->r1.x, system->r1.y, system->r1.z);
+	printf("# initial velocity: (%1.8f, %1.8f, %1.8f)\n", system->v1.x, system->v1.y, system->v1.z);
+	printf("# [particle 2]\n");
+	printf("# mass:             %1.8f\n", system->m2);
+	printf("# initial position: (%1.8f, %1.8f, %1.8f)\n", system->r2.x, system->r2.y, system->r2.z);
+	printf("# initial velocity: (%1.8f, %1.8f, %1.8f)\n", system->v2.x, system->v2.y, system->v2.z);
+	printf("# [particle 3]\n");
+	printf("# mass:             %1.8f\n", system->m3);
+	printf("# initial position: (%1.8f, %1.8f, %1.8f)\n", system->r3.x, system->r3.y, system->r3.z);
+	printf("# initial velocity: (%1.8f, %1.8f, %1.8f)\n", system->v3.x, system->v3.y, system->v3.z);
+	printf("\n");
 }
 
 int main()
@@ -27,5 +46,6 @@ int main()
 		.r3 = { 11, 0, 0},
 		.v3 = { 0, 0.24, 0},
 	};
+	print_profile(&system);
 	run(&system, expand_leap_frog, print_centroid);
 }
